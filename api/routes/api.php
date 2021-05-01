ef535bfe64262f78d26708acc06de5232df30253
jvrcavalcanti <jonnyvictor01@gmail.com>
2021-05-01 02:19:07 -0300

    I come back baby, update packages composer and npm

--- api/routes/api.php
+++ api/routes/api.php
@@ -14,3 +14,39 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/', fn() => response()->json(['message' => 'Welcome to API Távola Redonda.']));
+
+// Auth
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('login', [AuthController::class, 'login'])->name('auth.login');
+    Route::
+        middleware(['auth:sanctum'])
+        ->get('check', fn() => response()->json(['message' => 'Token valid']));
+});
+
+// Tag
+Route::prefix('tags')->group(function () {
+    Route::get('/', [TagController::class, 'index'])->name('tags.index');
+    Route::get('/{tag}', [TagController::class, 'show'])->where('id', '[0-9]+')->name('tags.show');
+});
+
+Route::prefix('posts')->group(function () {
+    Route::get('/{post}', [PostController::class, 'show']);
+    Route::get('/', [PostController::class, 'index']);
+});
+
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+    // Post
+    Route::prefix('posts')->group(function () {
+        Route::post('/create', [PostController::class, 'create']);
+        Route::delete('/delete/{post}', [PostController::class, 'delete']);
+        Route::put('/like/{post}/{value}', [PostController::class, 'like']);
+        Route::get('/like/{post}', [PostController::class, 'likeStatus']);
+    });
+});
+
+Route::fallback(fn() => response()->json([
+    'message' => 'Not Found'
+], 404));
--- api/routes/web.php
+++ api/routes/web.php
@@ -15,39 +15,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::prefix('api/v1')->group(function () {
-    // Auth
-    Route::prefix('auth')->group(function () {
-        Route::post('register', [AuthController::class, 'register'])->name('auth.register');
-        Route::post('login', [AuthController::class, 'login'])->name('auth.login');
-        Route::
-            middleware(['auth:sanctum'])
-            ->get('check', fn() => response()->json(['message' => 'Token valid']));
-    });
-
-    // Tag
-    Route::prefix('tags')->group(function () {
-        Route::get('/', [TagController::class, 'index'])->name('tags.index');
-        Route::get('/{tag}', [TagController::class, 'show'])->where('id', '[0-9]+')->name('tags.show');
-    });
-
-    Route::prefix('posts')->group(function () {
-        Route::get('/{post}', [PostController::class, 'show']);
-        Route::get('/', [PostController::class, 'index']);
-    });
-});
-
-Route::middleware('auth:sanctum')->prefix('api/v1')->group(function () {
-    // Post
-    Route::prefix('posts')->group(function () {
-        Route::post('/create', [PostController::class, 'create']);
-        Route::delete('/delete/{post}', [PostController::class, 'delete']);
-        Route::put('/like/{post}/{value}', [PostController::class, 'like']);
-        Route::get('/like/{post}', [PostController::class, 'likeStatus']);
-    });
-});
-
-Route::fallback(fn() => response()->json([
-    'message' => 'Not Found'
-], 404));
